d00789a7cde830c112d208dc9cd24edf07d5e15b
Syed Waqas <waqas.shah.syed@gmail.com>
2018-01-23 12:29:40 +0500

    Adding a new post

--- src/ForumApp.Frontend/ForumApp.Frontend/Scripts/Controllers/PostsController.js
+++ src/ForumApp.Frontend/ForumApp.Frontend/Scripts/Controllers/PostsController.js
@@ -3,12 +3,25 @@
 var app = angular.module('app');
 
 app.controller('postsController', [
-    '$scope', '$state', 'todoListService', 'NgTableParams', '$filter', 'authService',
-    function ($scope, $state, todoListService, NgTableParams, $filter, authService) {
+    '$scope', '$state', 'postsService', 'NgTableParams', '$filter', 'authService',
+    function ($scope, $state, postsService, NgTableParams, $filter, authService) {
     
         authService.fillAuthData();
         if (authService.authentication.isAuth) {
             $scope.email = authService.authentication.email;
+        }
+
+        $scope.submitNewPost = function () {
+            var post = {
+                Title: $scope.title,
+                Description: $scope.description,
+                Category: $scope.category
+            };
+            postsService.submitNewPost(post).success(function(response) {
+                $state.go('posts');
+            }).error(function(error) {
+                $scope.errorMessage = "Error while submitting new Post. Please try again later";
+            });
         }
         
         $scope.addNewPost = function() {
---
+++ src/ForumApp.Frontend/ForumApp.Frontend/Scripts/Services/PostsService.js
@@ -0,0 +1,51 @@
+﻿var app = angular.module('app');
+
+app.factory('postsService', ['$http', '$q', 'globalService', function ($http, $q, globalService) {
+    return {
+        getPostsByEmail: function (searchParameters) {
+            return $http.get(globalService.serverUrl + 'post', { params: searchParameters })
+                .success(function (response) {
+                    return response;
+                })
+                .error(function (error) {
+                    return error;
+                });
+        },
+        getPost: function (searchParameters) {
+            return $http.get(globalService.serverUrl + 'post', { params: searchParameters })
+                .success(function (response) {
+                    return response;
+                })
+                .error(function (error) {
+                    return error;
+                });
+        },
+        submitNewPost: function(todo) {
+            return $http.post(globalService.serverUrl + 'post', todo)
+                .success(function (response) {
+                    return response;
+                })
+                .error(function (error) {
+                    return error;
+                });
+        },
+        updatePost: function (todo) {
+            return $http.put(globalService.serverUrl + 'post', todo)
+                .success(function (response) {
+                    return response;
+                })
+                .error(function (error) {
+                    return error;
+                });
+        },
+        deletePost: function (id) {
+            return $http.delete(globalService.serverUrl + 'post/' + id)
+                .success(function (response) {
+                    return response;
+                })
+                .error(function (error) {
+                    return error;
+                });
+        }
+    };
+}]);
